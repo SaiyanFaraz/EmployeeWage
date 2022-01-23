1a5d479bfa9276baa7619d33c26df54dcd532a70
Shabuddin <sfsaiyanfaraz@gmail.com>
2022-01-23 17:26:49 +0530

    UC3/PartTime

--- EmployeeWageProg.java
+++ EmployeeWageProg.java
@@ -1,17 +1,25 @@
 public class EmployeeWageProg 
 {
 			public static final int IS_FULL_TIME = 1;
+			public static final int IS_PART_TIME = 2;
 			public static final int WAGE_PER_HOUR=20;
 			
 	public static void main(String[] args)
 	{
 		int attendance, perDayWage=0,workedHours=0;
 		System.out.println("Welcome to Employee Wage Computation");
-		attendance=(int)Math.floor(Math.random()*10)%2;
+		attendance=(int)Math.floor(Math.random()*10)%3;
 		if(attendance==IS_FULL_TIME) 
 		{
 			System.out.println("The Employee is present");
 			workedHours=8;
+			perDayWage=WAGE_PER_HOUR*workedHours;
+			System.out.println("Employee wage is: "+ perDayWage);
+		}
+		else if(attendance==IS_PART_TIME) 
+		{
+			System.out.println("The Employee is present Half a day");
+			workedHours=4;
 			perDayWage=WAGE_PER_HOUR*workedHours;
 			System.out.println("Employee wage is: "+ perDayWage);
 		}
