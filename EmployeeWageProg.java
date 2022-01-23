1084a6a43cb613b598d0bfa75576a53e32af2c7b
Shabuddin <sfsaiyanfaraz@gmail.com>
2022-01-23 17:14:12 +0530

    UC2/DailyWage

--- EmployeeWageProg.java
+++ EmployeeWageProg.java
@@ -1,19 +1,24 @@
 public class EmployeeWageProg 
 {
 			public static final int IS_FULL_TIME = 1;
+			public static final int WAGE_PER_HOUR=20;
 			
 	public static void main(String[] args)
 	{
-		int attendance;
+		int attendance, perDayWage=0,workedHours=0;
 		System.out.println("Welcome to Employee Wage Computation");
 		attendance=(int)Math.floor(Math.random()*10)%2;
 		if(attendance==IS_FULL_TIME) 
 		{
 			System.out.println("The Employee is present");
+			workedHours=8;
+			perDayWage=WAGE_PER_HOUR*workedHours;
+			System.out.println("Employee wage is: "+ perDayWage);
 		}
 		else
 		{
 			System.out.println("Employee is absent");
+			workedHours=0;
 		}
 	}	 
 } 
