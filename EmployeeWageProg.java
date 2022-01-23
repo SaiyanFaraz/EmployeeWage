c9c2865faf1cc206761d314b470ed1cc29625116
Shabuddin <sfsaiyanfaraz@gmail.com>
2022-01-23 15:23:06 +0530

    UC1/added random

--- EmployeeWageProg.java
+++ EmployeeWageProg.java
@@ -1,11 +1,19 @@
-
 public class EmployeeWageProg 
 {
+			public static final int IS_FULL_TIME = 1;
 			
 	public static void main(String[] args)
 	{
-		
+		int attendance;
 		System.out.println("Welcome to Employee Wage Computation");
-	
+		attendance=(int)Math.floor(Math.random()*10)%2;
+		if(attendance==IS_FULL_TIME) 
+		{
+			System.out.println("The Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}	 
 } 
