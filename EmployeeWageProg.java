1ea69f86737ee642f4fedd540e4a0c5e3450c160
Shabuddin <sfsaiyanfaraz@gmail.com>
2022-02-08 02:58:59 +0530

    UC7/added_method

--- EmployeeWageProg.java
+++ EmployeeWageProg.java
@@ -2,16 +2,16 @@ public class EmployeeWageProg
 {
 			public static final int IS_FULL_TIME = 1;
 			public static final int IS_PART_TIME = 2;
-			public static final int WAGE_PER_HOUR=20;
-			public static final int MAX_WORKING_DAYS=20;
-			public static final int MAX_WORKING_HOURS=100;
-			
-	public static void main(String[] args)
+//			public static final int WAGE_PER_HOUR=20;
+//			public static final int MAX_WORKING_DAYS=20;
+//			public static final int MAX_WORKING_HOURS=100;
+		
+	public static int calcEmpWageForCompany(int wagePerHour, int maxDays,int maxHours)
 	{
 		int attendance, perDayWage=0,workedHours=0,day=0;
 		int totalAttendance=0,totalWorkedHours=0;
 		System.out.println("Welcome to Employee Wage Computation");
-		while(totalAttendance<=MAX_WORKING_DAYS && totalWorkedHours<=MAX_WORKING_HOURS)
+		while(totalAttendance<=maxDays && totalWorkedHours<maxHours)
 			{
 			day=day+1;
 			System.out.print("Day "+day+",");
@@ -22,7 +22,7 @@ public class EmployeeWageProg
 						{
 							System.out.println(" The Employee is present");
 							workedHours=8;
-							perDayWage=WAGE_PER_HOUR*workedHours;
+							perDayWage=wagePerHour*workedHours;
 							System.out.println("Employee wage is: "+ perDayWage);
 							break;
 						}
@@ -30,7 +30,7 @@ public class EmployeeWageProg
 						{
 							System.out.println(" The Employee is present Half a day");
 							workedHours=4;
-							perDayWage=WAGE_PER_HOUR*workedHours;
+							perDayWage=wagePerHour*workedHours;
 							System.out.println("Employee wage is: "+ perDayWage);
 							break;
 						}
@@ -39,7 +39,7 @@ public class EmployeeWageProg
 							System.out.println(" Employee is absent");
 							workedHours=0;
 							break;
-						}
+						} 
 				}
 				if(workedHours==8 || workedHours==4)
 				{
@@ -49,7 +49,13 @@ public class EmployeeWageProg
 				totalWorkedHours=totalWorkedHours+workedHours;
 				System.out.println("Total worked hours: "+ totalWorkedHours);
 			}
-		int totalEmpWage= totalWorkedHours*WAGE_PER_HOUR;
+		int totalEmpWage= totalWorkedHours*wagePerHour;
 		System.out.println("Total earnings for the employee: "+ totalEmpWage);
+		
+		return totalEmpWage;
+	}
+	public static void main(String[] args)
+	{
+		int totalWage = calcEmpWageForCompany(20,2,10);
 	}
 }
