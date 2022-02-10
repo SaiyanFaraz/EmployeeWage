95a727f9d8933f0908a6f7c91b42e0a509931dfb
Shabuddin <sfsaiyanfaraz@gmail.com>
2022-02-11 01:46:54 +0530

    Total Employee wage for each company

--- EmployeeWageProg.java
+++ EmployeeWageProg.java
@@ -1,63 +1,72 @@
-public class EmployeeWageProg 
+public class EmployeeWageProg
 {
 			public static final int IS_FULL_TIME = 1;
 			public static final int IS_PART_TIME = 2;
-//			public static final int WAGE_PER_HOUR=20;
-//			public static final int MAX_WORKING_DAYS=20;
-//			public static final int MAX_WORKING_HOURS=100;
 		
-	public static int calcEmpWageForCompany(String company, int wagePerHour, int maxDays,int maxHours)
-	{
-		int attendance, perDayWage=0,workedHours=0,day=0;
-		int totalAttendance=0,totalWorkedHours=0;
-		System.out.println("Welcome to Employee Wage Computation");
-		while(totalAttendance<=maxDays && totalWorkedHours<maxHours)
+			private final String company;
+			private final int wagePerHour;
+			private final int maxDays;
+			private final int maxHours;
+			
+	public EmployeeWageProg(String company, int wagePerHour, int maxDays,int maxHours) {
+			this.company = company;
+			this.wagePerHour = wagePerHour;
+			this.maxDays = maxDays;
+			this.maxHours = maxHours;
+	}
+	private int calcEmpWage() {
+			
+	//		Variables
+	int attendance, perDayWage=0,workedHours=0,day=0;
+	int totalAttendance=0,totalWorkedHours=0;
+	
+	//computation
+	while(totalAttendance <= maxDays  && totalWorkedHours < maxHours)
+		{
+		day=day+1;
+		System.out.print("Day "+day+",");
+		attendance=(int)Math.floor(Math.random()*10)%3;
+		switch(attendance)
 			{
-			day=day+1;
-			System.out.print("Day "+day+",");
-			attendance=(int)Math.floor(Math.random()*10)%3;
-			switch(attendance)
-				{
-					case IS_FULL_TIME: 
-						{
-							System.out.println(" The Employee is present");
-							workedHours=8;
-							perDayWage=wagePerHour*workedHours;
-							System.out.println("Employee wage is: "+ perDayWage);
-							break;
-						}
-					case IS_PART_TIME: 
-						{
-							System.out.println(" The Employee is present Half a day");
-							workedHours=4;
-							perDayWage=wagePerHour*workedHours;
-							System.out.println("Employee wage is: "+ perDayWage);
-							break;
-						}
-					default:
-						{
-							System.out.println(" Employee is absent");
-							workedHours=0;
-							break;
-						} 
-				}
-				if(workedHours==8 || workedHours==4)
-				{
+				case IS_FULL_TIME: 
+					{
+						System.out.println(" The Employee is present");
+						workedHours=8;
+						perDayWage=wagePerHour*workedHours;
+						System.out.println("Employee wage is: "+ perDayWage);
+						break;
+					}
+				case IS_PART_TIME: 
+					{
+						System.out.println(" The Employee is present Half a day");
+						workedHours=4;
+						perDayWage=wagePerHour*workedHours;
+						System.out.println("Employee wage is: "+ perDayWage);
+						break;
+					}
+				default:
+					{
+						System.out.println(" Employee is absent");
+						workedHours=0;
+						break;
+					} 
+			}		
 				totalAttendance++;
-				System.out.println("Total attendance:"+ totalAttendance);
-				}
-				totalWorkedHours=totalWorkedHours+workedHours;
-				System.out.println("Total worked hours: "+ totalWorkedHours);
-			}
-		int totalEmpWage= totalWorkedHours*wagePerHour;
-		System.out.println("Total earnings for the employee for the company : "+company+" is: "+ totalEmpWage);
-		
+				totalWorkedHours = totalWorkedHours + workedHours;
+			System.out.println("Total attendance:"+ totalAttendance);
+			System.out.println("Total worked hours: "+ totalWorkedHours);
+		}
+		int totalEmpWage = totalWorkedHours * wagePerHour;
 		return totalEmpWage;
+		}
+	
+		public static void main(String[] args) {
+				
+			EmployeeWageProg dMart = new EmployeeWageProg("DMart",20,2,10);
+			EmployeeWageProg bigBasket = new EmployeeWageProg("BigBasket",20,2,10);
+			
+			System.out.println("Total earnings for the employee for the company : "+ dMart.company+" is: "+ dMart.calcEmpWage());
+			System.out.println("Total earnings for the employee for the company : "+ bigBasket.company+" is: "+ bigBasket.calcEmpWage());
+	
+		}
 	}
-	public static void main(String[] args)
-	{
-		calcEmpWageForCompany("DMart",20,2,10);
-		calcEmpWageForCompany("Relaince",10,4,20);
-
-	}
-}
